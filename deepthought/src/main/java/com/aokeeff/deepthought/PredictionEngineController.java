c7f0f3934396d60eedac7cb061ce5549df78c5ea
aokeeff <alexwokeeffe@gmail.com>
2016-12-04 20:57:12 +0000

    Added deepthought module for predictions

---
+++ deepthought/src/main/java/com/aokeeff/deepthought/PredictionEngineController.java
@@ -0,0 +1,21 @@
+package com.aokeeff.deepthought;
+
+import com.aokeeff.cassini.model.StatsPack;
+
+/**
+ * Created by aokeeff on 04/12/2016.
+ */
+public class PredictionEngineController {
+
+    private final StatsPack statsPack;
+
+    public PredictionEngineController(StatsPack statsPack) {
+        this.statsPack = statsPack;
+    }
+
+    public void predictResults() {
+
+    }
+
+
+}
